d5cad60016286be4fd6598a96ac9a38133a96629
scrap <scrap@5-63-158-231.cloudvps.regruhosting.ru>
2021-05-04 23:04:03 +0300

    update for start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
+echo Create output folder
+mkdir output
 echo Start nightmare over xvfb-run 
-DEBUG=nightmare:*,electron:* xvfb-run --server-args="-screen 0 1280x2000x24" npm start
+#DEBUG=nightmare:*,electron:* xvfb-run --server-args="-screen 0 1280x2000x24" npm start
+xvfb-run --server-args="-screen 0 1280x1000x24" npm start
